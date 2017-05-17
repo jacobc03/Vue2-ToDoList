a27e7648888b1bf70fb9018cec9c298749054bc1
Jacob <jacob@Jacobs-MacBook-Air.local>
2017-05-16 22:41:26 -0400

    looping and rendering data to page

--- todo-app/src/components/TodoList.vue
+++ todo-app/src/components/TodoList.vue
@@ -1,10 +1,31 @@
 <template>
 	<div>
-		<ul>
-			<li> Todo A </li>
-			<li> Todo B </li>
-			<li> Todo C </li>
-		</ul>
+		<!-- JavaSctipt expression in Vue are enclosed in double curly brackets -->
+		<!-- The following 2 lines gives a count of how many todos items have a done value of true or false -->
+		<p>Completed Tasks:{{todos.filter(todo => {return todo.done === true}).length}}</p>
+		<p>Pending Tasks:{{todos.filter(todo => {return todo.done === false}).length}}</p>
+		<div class="ui centered card" v-for="todo in todos">
+			<div class="content">
+				<div class="header">
+				<!-- displays the title for each todos item -->
+					{{ todo.title }}
+				</div>
+				<div class="meta">
+					{{ todo.project }}
+				</div>
+				<div class="extra content">
+					<span class="right floated edit icon">
+						<i class="edit icon"></i>
+					</span>
+				</div>
+			</div>
+			<div class="ui bottom attached green basic button" v-show="todo.done">
+				Completed
+			</div>
+			<div class="ui bottom attached red basic button" v-show="!todo.done">
+				Complete
+			</div>
+		</div>
 	</div>
 </template>
 
