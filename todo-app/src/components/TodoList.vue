79b8b7e4b4094b923c8bf9bc4a8cf90a9d1c4cf5
Jacob <jacob@Jacobs-MacBook-Air.local>
2017-05-16 22:21:03 -0400

    Unordered list appears on site

---
+++ todo-app/src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<ul>
+			<li> Todo A </li>
+			<li> Todo B </li>
+			<li> Todo C </li>
+		</ul>
+	</div>
+</template>
+
+<script type = "text/javascript">
+
+export default {
+  props: ['todos'],
+  components: {
+
+  }
+}
+</script>
+<style>
+	
+</style>
